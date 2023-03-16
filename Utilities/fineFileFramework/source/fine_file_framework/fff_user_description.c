903194724ef8331039f5660f75e0c0e80d60df04
Gabriel Wang <gabriel.wang@arm.com>
2023-03-16 21:37:15 +0000

    use incbin to replace assembly code

--- Utilities/fineFileFramework/source/fine_file_framework/fff_user_description.c
+++ Utilities/fineFileFramework/source/fine_file_framework/fff_user_description.c
@@ -74,85 +74,14 @@ enum {
 
 
 /*============================ GLOBAL VARIABLES ==============================*/
-extern const uint8_t HOTEL_MP3_ROM[];
-extern const uint32_t HOTEL_MP3_ROM_Length;
-
 /*============================ LOCAL VARIABLES ===============================*/
 /*============================ PROTOTYPES ====================================*/
 /*============================ IMPLEMENTATION ================================*/
 
+#define __FFF_USER_DEFINE_FILE_FRAMEWORK__
+#include "__fff_user_def_file_framework.c"
+#undef __FFF_USER_DEFINE_FILE_FRAMEWORK__
 
-def_fff(MPSx_Local_Disk,
-    use_fff_disk(disk_c,
-        arm_mem_file_node_t         hotel_mp3;
-    );
-    
-    use_fff_disk(disk_d,
-        use_fff_folder(symphony,
-            arm_mem_file_node_t     no_1_mp3;
-            arm_mem_file_node_t     no_2_mp3;
-            arm_mem_file_node_t     no_3_mp3;
-            arm_mem_file_node_t     no_4_mp3;
-        );
-    );
-)
-
-imp_fff(MPSx_Local_Disk,
-
-    fff_disk(disk_c, &MPSx_Local_Disk, disk_d,
-        fff_disk_path("C:"),
-        fff_list(
-            //! memory file: cannon.mp3, read-only
-            fff_mem_file(hotel_mp3, &MPSx_Local_Disk.disk_c, hotel_mp3, 
-                fff_path("hotel.mp3"),
-                fff_access(FFF_READ_ONLY),
-                
-                fff_mem_content(HOTEL_MP3_ROM, HOTEL_MP3_ROM_Length),
-            ),
-        ),
-    ),
-    
-    fff_disk(disk_d, &MPSx_Local_Disk, disk_d,
-        fff_disk_path("D:"),
-        fff_list(
-            fff_folder(symphony, &MPSx_Local_Disk.disk_d, symphony,
-                fff_folder_path(
-                    "symphony\\music\\beethoven", 
-                    "symphony/music/beethoven"
-                ),
-
-                fff_list(
-                    fff_mem_file(no_1_mp3, &MPSx_Local_Disk.disk_d.symphony, no_2_mp3, 
-                        fff_path("no_1.mp3"),
-                        fff_access(FFF_READ_WRITE),
-                    ),
-                    fff_mem_file(no_2_mp3, &MPSx_Local_Disk.disk_d.symphony, no_3_mp3, 
-                        fff_path("no_2.mp3"),
-                        fff_access(FFF_READ_WRITE),
-                    ),
-                    fff_mem_file(no_3_mp3, &MPSx_Local_Disk.disk_d.symphony, no_4_mp3, 
-                        fff_path("no_3.mp3"),
-                        fff_access(FFF_READ_WRITE),
-                    ),
-                    fff_mem_file(no_4_mp3, &MPSx_Local_Disk.disk_d.symphony, no_4_mp3, 
-                        fff_path("no_4.mp3"),
-                        fff_access(FFF_READ_WRITE),
-                    ),
-                ),
-            ),
-        ),
-    ),
-);
-
-
-__attribute__((used))
-char *_sys_command_string(char *cmd, int len)
-{
-    (void)cmd;
-    (void)len;
-    /* write a command line here, which will be passed to main */
-    return "--input_file hotel.mp3";
-}
 
 /*----------------------------------------------------------------------------*
  * Compiler Specific Code to run __vsf_main_entry() before main()             *
