9df096c78ac4cad08b63d394d685bdfc6ecc5a23
FabKlein <fabien.klein@arm.com>
2023-04-20 13:50:57 +0100

    Added DF1 Biquad filtering unit-tests

--- Kernels/ARM-Optimized/DSP/Source/FilteringFunctions/arm_filtering_tests/_utils.h
+++ Kernels/ARM-Optimized/DSP/Source/FilteringFunctions/arm_filtering_tests/_utils.h
@@ -1,3 +1,31 @@
+/* ----------------------------------------------------------------------
+ * Project:      CMSIS DSP Library
+ * Title:        _utils.h
+ * Description:  Test optimized filtering functions against CMSIS DSP reference
+ *
+ * $Date:        20. Apr 2023
+ * $Revision:    V1.0.0
+ *
+ * Target Processor: Cortex-M cores
+ * -------------------------------------------------------------------- */
+/*
+ * Copyright (C) 2023 ARM Limited or its affiliates. All rights reserved.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * Licensed under the Apache License, Version 2.0 (the License); you may
+ * not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an AS IS BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 #ifndef UTILS_H
 #define UTILS_H
 
@@ -12,7 +40,7 @@
 
 #define PRINT(fmt, args...)
 
-#define MAX_BUF_SZ              (1024*2)
+#define MAX_BUF_SZ              (1024*1)
 
 #define NO_DIFF_ALLOWED         0
 #define MAX_ABSDIFF_ALLOWED     20
@@ -25,8 +53,7 @@
 // extra length to ensure vector operation did not corrupt the tail
 #define EXTRA_TAIL_CHK          8
 
-typedef enum
-{
+typedef enum {
     RAMP,
     RAND,
     SINE,
@@ -36,7 +63,7 @@ typedef enum
     RAND2
 } inputTyp;
 
-static int seed = 0xdeadbeef;
+static int      seed = 0xdeadbeef;
 
 static inline void set_pseudo_rand_seed(int32_t the_seed)
 {
@@ -60,7 +87,7 @@ static inline int32_t pseudo_rand(int32_t seed)
 
 #define SIN250_48_PERIOD 96*2
 
-static short     sin250_48[SIN250_48_PERIOD] = {
+static short    sin250_48[SIN250_48_PERIOD] = {
     0, 536, 1072, 1606, 2139, 2669, 3196, 3720,
     4240, 4756, 5266, 5771, 6270, 6762, 7246, 7723,
     8192, 8652, 9102, 9543, 9974, 10394, 10803, 11200,
@@ -177,20 +204,30 @@ static short     sin250_48[SIN250_48_PERIOD] = {
        PRINT("maxdiff %d\n",(int)maxdiff);                                          \
 }
 
+/* Scaling for intermediate accumulating buffer */
+/* Q31 FIR is using 64-bit accumulation, requiring 2x amount of blocksze for storage */
+
+#define SZ_SCL_f32 1
+#define SZ_SCL_f16 1
+#define SZ_SCL_q31 2
+#define SZ_SCL_q15 1
+
 #define SETUP_FIR(TYP, EXT, blockSize, numTaps)                                        \
     arm_fir_instance_##EXT SOPT, SREF;                                                 \
     ALIGN16 TYP     Coeffs[numTaps + 16], *pCoeffs;                                    \
     ALIGN16 TYP     Input[MAX_BUF_SZ], *pInput;                                        \
-    ALIGN16 TYP     firStateOPT[2*MAX_BUF_SZ + MAX_BUF_SZ + EXTRA_TAIL_CHK] = {0},       \
+    ALIGN16 TYP     firStateOPT[CAT(SZ_SCL_,EXT)*MAX_BUF_SZ +                          \
+                                               MAX_BUF_SZ + EXTRA_TAIL_CHK] = {0},     \
         *pfirStOPT;                                                                    \
-    ALIGN16 TYP     firStateREF[2*MAX_BUF_SZ + MAX_BUF_SZ + EXTRA_TAIL_CHK] = {0},       \
+    ALIGN16 TYP     firStateREF[CAT(SZ_SCL_,EXT)*MAX_BUF_SZ +                          \
+                                               MAX_BUF_SZ + EXTRA_TAIL_CHK] = {0},     \
         *pfirStREF;                                                                    \
     ALIGN16 TYP     dstOPT[MAX_BUF_SZ + EXTRA_TAIL_CHK] = {0}, *pdstOPT;               \
     ALIGN16 TYP     dstREF[MAX_BUF_SZ + EXTRA_TAIL_CHK] = {0}, *pdstREF;               \
                                                                                        \
     if (!numTaps || !blockSize) {                                                      \
         printf("invalid taps / blocksz\n"); exit(1);}                                  \
-    PRINT("Test : blockSize %d numTaps %d off %d\n", blockSize, numTaps, alignOffs);   \
+    PRINT("Test: blockSize %d Taps %d off %d\n", blockSize, numTaps, alignOffs);  \
     seed += blockSize + numTaps;                                                       \
     pCoeffs     = Coeffs;                                                              \
     pfirStOPT   = firStateOPT + alignOffs;                                             \
@@ -288,5 +325,157 @@ static short     sin250_48[SIN250_48_PERIOD] = {
             dump_buf_matlab(pdstScal, CMPLX_DIM*vectorSize + EXTRA_TAIL_CHK, 16, format);       \
             dump_buf_matlab(pdstVec, CMPLX_DIM*vectorSize + EXTRA_TAIL_CHK, 16, format);        \
         }
+
+#define POSTSHIFT_q31 ,10
+#define POSTSHIFT_q15 ,1
+
+#define POSTSHIFT_f32
+#define POSTSHIFT_f16
+
+#define MAX_BIQ_STAGES      16
+
+#define SETUP_BIQUAD_DF1(TYP, EXT,nbCascade, blockSize,alignOffs)                               \
+    ALIGN16 TYP     stateRef[4 * MAX_BIQ_STAGES] = { 0 }, *pstateRef;                           \
+    ALIGN16 TYP     stateOpt[4 * MAX_BIQ_STAGES] = { 0 }, *pstateOpt;                           \
+    ALIGN16 TYP     coefs[5 * nbCascade], *pcoefs = coefs;                                      \
+    arm_biquad_casd_df1_inst_##EXT SRef, SOpt;                                                  \
+    ALIGN16 TYP Input[MAX_BUF_SZ], *pInput = Input;                                             \
+    ALIGN16 TYP dstRef[MAX_BUF_SZ + EXTRA_TAIL_CHK] = { 0 },                                    \
+     *pdstRef = dstRef + alignOffs;                                                             \
+    ALIGN16 TYP dstOpt[MAX_BUF_SZ + EXTRA_TAIL_CHK] = { 0 },                                    \
+    *pdstOpt = dstOpt + alignOffs;                                                              \
+    pstateRef = stateRef + alignOffs;                                                           \
+    pstateOpt = stateOpt + alignOffs;
+
+
+
+
+#define GEN_BIQUAD_LPF_FLT(TYP, nbCascade, pcoefs, scale)                                       \
+    for (int i = 0; i < nbCascade; i++) {                                                       \
+        float32_t w, f0, alpha;                                                                 \
+        float32_t a0, a1, a2, b0, b1, b2, Q;                                                    \
+                                                                                                \
+        /* low pass filter, fci = 200*i/8000 */                                                 \
+        Q = 2.0f;                                                                               \
+        f0 = 200.f * (float32_t)(i + 1);                                                        \
+        w = 2 * PI * f0 / 8000.0f;                                                              \
+        alpha = sinf(w) / 2 * Q;                                                                \
+                                                                                                \
+        a0 = 1.0f + alpha;                                                                      \
+        b0 = ((1 - cosf(w)) / 2.f) / a0;                                                        \
+        b1 = (2.0f * b0);                                                                       \
+        b2 = b0;                                                                                \
+                                                                                                \
+        a0 = 1.0f + alpha;                                                                      \
+        a1 = -2.0f * cosf(w) / a0;                                                              \
+        a2 = (1.0f - alpha) / a0;                                                               \
+                                                                                                \
+        PRINT("coef a1=%f a2=%f b0=%f b1=%f b2=%f \n", a1, a2, b0, b1, b0);                     \
+                                                                                                \
+        *pcoefs++ = (TYP)b0;                                                                    \
+        *pcoefs++ = (TYP)b1;                                                                    \
+        *pcoefs++ = (TYP)b2;                                                                    \
+        *pcoefs++ = -(TYP)a1;                                                                   \
+        *pcoefs++ = -(TYP)a2;                                                                   \
+    }
+
+
+
+#define INSERT_0_q31_t
+#define INSERT_0_q15_t    *pcoefs++ = 0;
+
+
+/* Biquad Low pass filter Gen for fixed point  */
+#define GEN_BIQUAD_LPF_FX(TYP, nbCascade, pcoefs,scale)                                             \
+    for (int i = 0; i < nbCascade; i++) {                                                           \
+        float32_t w, f0, alpha;                                                                     \
+        float32_t a0, a1, a2, b0, b1, b2, Q;                                                        \
+        TYP a1fx, a2fx, b0fx, b1fx, b2fx;                                                           \
+                                                                                                    \
+        /* low pass filter, fci = 200*i/8000 */                                                     \
+        Q = 2.0f;                                                                                   \
+        f0 = 400.0f + 100.0f * (float32_t)(i + 1);                                                  \
+        w = 2 * PI * f0 / 8000.0f;                                                                  \
+        alpha = sinf(w) / 2.0f * Q;                                                                 \
+                                                                                                    \
+        a0 = 1.0f + alpha;                                                                          \
+        b0 = ((1 - cosf(w)) / 2.0f) / a0;                                                           \
+        b1 = (2.0f * b0);                                                                           \
+        b2 = b0;                                                                                    \
+                                                                                                    \
+        a0 = (1.0f + alpha);                                                                        \
+        a1 = -2.0f * cosf(w) / a0;                                                                  \
+        a2 = (1.0f - alpha) / a0;                                                                   \
+                                                                                                    \
+        a1fx = (TYP)(roundf(a1 * powf(2.0, scale)));                                                \
+        a2fx = (TYP)(roundf(a2 * powf(2.0, scale)));                                                \
+        b0fx = (TYP)(roundf(b0 * powf(2.0, scale)));                                                \
+        b1fx = (TYP)(roundf(b1 * powf(2.0, scale)));                                                \
+        b2fx = (TYP)(roundf(b2 * powf(2.0, scale)));                                                \
+                                                                                                    \
+        PRINT("coef a1=%f a2=%f b0=%f b1=%f b2=%f : %d %d %d %d %d\n",                              \
+        a1, a2, b0, b1, b0, a1fx, a2fx, b0fx, b1fx, b2fx);                                          \
+                                                                                                    \
+        *pcoefs++ = b0fx;                                                                           \
+        /* special case for Q15 */                                                                  \
+        /* Coefficients are stored in the following order:  */                                      \
+        /* {b10, 0, b11, b12, a11, a12, b20, 0, b21,...}    */                                      \
+        INSERT_0_##TYP                                                                              \
+        *pcoefs++ = b1fx;                                                                           \
+        *pcoefs++ = b2fx;                                                                           \
+        *pcoefs++ = a1fx;                                                                           \
+        *pcoefs++ = a2fx;                                                                           \
+    }
+
+
+
+#define hasNonNormalElts(a, buf_sz )                                                            \
+{                                                                                               \
+    int cntNonNormal = 0;                                                                       \
+    for (int i = 0; i < buf_sz; i++) {                                                          \
+        float32_t __a = (float32_t)a[i];                                                        \
+        /*neither infinity, NaN, zero or subnormal */                                           \
+        if(!isnormal(__a))   {                                                                  \
+            printf("idx %d %f \n",i, a[i]);                                                     \
+            cntNonNormal++; }                                                                   \
+    }                                                                                           \
+    if(cntNonNormal) printf("!!! %s unexpected elts cnt = %d\n",                                \
+                #a, cntNonNormal);                                                              \
+}
+
+
+
+
+#define RUN_BIQUAD_DF1(fct, thresh)                                                             \
+    int64_t         ref, opt;                                                                   \
+    ref = get_system_ticks();                                                                   \
+    fct(&SRef, pInput, pdstRef, blockSize);                                                     \
+    ref = get_system_ticks() - ref;                                                             \
+                                                                                                \
+    opt = get_system_ticks();                                                                   \
+    ADD_FCT_SUFFIX(fct,_mve)(&SOpt, pInput, pdstOpt, blockSize);                                \
+    opt = get_system_ticks() - opt;                                                             \
+                                                                                                \
+    p->ref = ref;                                                                               \
+    p->opt = opt;                                                                               \
+                                                                                                \
+    check_for_error(pdstRef, pdstOpt, (blockSize + EXTRA_TAIL_CHK),                             \
+    ( __typeof__ (Input[0]))(thresh));                                                          \
+    check_for_error(stateRef, stateOpt, (4 * nbCascade + EXTRA_TAIL_CHK),                       \
+    ( __typeof__ (Input[0]))(thresh));
+
+
+#define DUMP_ON_BIQUAD_DF1_ERROR(format)                                                        \
+    if (errorCnt || dump)                                                                       \
+    {                                                                                           \
+        dump_buf_matlab(pInput, blockSize, 8, format);                                          \
+        dump_buf_matlab(pdstRef, blockSize, 8, format);                                         \
+        dump_buf_matlab(pdstOpt, blockSize, 8, format);                                         \
+        dump_buf_matlab(stateRef, (4 * nbCascade), 8, format);                                  \
+        dump_buf_matlab(stateOpt, (4 * nbCascade), 8, format);                                  \
+    }
+
+
+
 
 #endif
--- Kernels/ARM-Optimized/DSP/Source/FilteringFunctions/arm_filtering_tests/arm_filtering_tests.c
+++ Kernels/ARM-Optimized/DSP/Source/FilteringFunctions/arm_filtering_tests/arm_filtering_tests.c
@@ -3,8 +3,8 @@
  * Title:        arm_filter_tests.c
  * Description:  Test optimized filtering functions against CMSIS DSP reference
  *
- * $Date:        06. Apr 2023
- * $Revision:    V1.0.0
+ * $Date:        20. Apr 2023
+ * $Revision:    V1.0.1
  *
  * Target Processor: Cortex-M cores
  * -------------------------------------------------------------------- */
@@ -59,6 +59,130 @@ void SysTick_Handler(void)
 #endif
 }
 
+
+
+
+int test_biq_df1_f32(int blockSize, int nbCascade, int dump, prof * p)
+{
+    int             errorCnt = 0;
+#if defined(ARM_MATH_MVEF)
+    int             alignOffs = 0;
+
+    SETUP_BIQUAD_DF1(float32_t, f32, nbCascade, blockSize, alignOffs);
+
+    pcoefs = coefs;
+    GEN_BIQUAD_LPF_FLT(float32_t, nbCascade, pcoefs, 0);
+
+
+    ALIGN16 arm_biquad_mod_coef_f32 coefsModRef[nbCascade];
+    ALIGN16 arm_biquad_mod_coef_f32 coefsModOpt[nbCascade];
+
+    arm_biquad_cascade_df1_mve_init_f32(&SRef, nbCascade, coefs, coefsModRef, pstateRef);
+    arm_biquad_cascade_df1_mve_init_f32(&SOpt, nbCascade, coefs, coefsModOpt, pstateOpt);
+
+    set_input(RAND, blockSize, pInput, powf(2.0f, -30.0f), float32_t);
+
+    RUN_BIQUAD_DF1(arm_biquad_cascade_df1_f32, 1e-6f);
+
+    DUMP_ON_BIQUAD_DF1_ERROR("%f");
+
+    hasNonNormalElts(pdstRef, blockSize);
+    hasNonNormalElts(pdstOpt, blockSize);
+#else
+    printf("skipped\n");
+#endif
+    return errorCnt;
+}
+
+
+
+
+int test_biq_df1_f16(int blockSize, int nbCascade, int dump, prof * p)
+{
+    int             errorCnt = 0;
+#if defined(ARM_MATH_MVEF)
+    int             alignOffs = 0;
+
+    SETUP_BIQUAD_DF1(float16_t, f16, nbCascade, blockSize, alignOffs);
+
+    pcoefs = coefs;
+    GEN_BIQUAD_LPF_FLT(float16_t, nbCascade, pcoefs, 0);
+
+
+    ALIGN16 arm_biquad_mod_coef_f16 coefsModRef[nbCascade];
+    ALIGN16 arm_biquad_mod_coef_f16 coefsModOpt[nbCascade];
+
+    arm_biquad_cascade_df1_mve_init_f16(&SRef, nbCascade, coefs, coefsModRef, pstateRef);
+    arm_biquad_cascade_df1_mve_init_f16(&SOpt, nbCascade, coefs, coefsModOpt, pstateOpt);
+
+    set_input(RAND, blockSize, pInput, powf(2.0f, -30.0f), float16_t);
+
+    RUN_BIQUAD_DF1(arm_biquad_cascade_df1_f16, 1e-2f);
+
+    DUMP_ON_BIQUAD_DF1_ERROR("%f");
+
+    hasNonNormalElts(pdstRef, blockSize);
+    hasNonNormalElts(pdstOpt, blockSize);
+#else
+    printf("skipped\n");
+#endif
+    return errorCnt;
+}
+
+
+
+int test_biq_df1_q31(int blockSize, int nbCascade, int dump, prof * p)
+{
+    int             errorCnt = 0;
+#if defined(ARM_MATH_MVEF)
+    int             alignOffs = 0;
+
+    SETUP_BIQUAD_DF1(q31_t, q31, nbCascade, blockSize, alignOffs);
+
+    arm_biquad_cascade_df1_init_q31(&SRef, nbCascade, pcoefs, pstateRef POSTSHIFT_q31);
+    arm_biquad_cascade_df1_init_q31(&SOpt, nbCascade, pcoefs, pstateOpt POSTSHIFT_q31);
+
+    pcoefs = coefs;
+    GEN_BIQUAD_LPF_FX(q31_t, nbCascade, pcoefs, 20);
+
+    set_input(RAND, blockSize, pInput, powf(2.0, -10.0), q31_t);
+
+    RUN_BIQUAD_DF1(arm_biquad_cascade_df1_q31, 1);
+
+    DUMP_ON_BIQUAD_DF1_ERROR("%d");
+
+#else
+    printf("skipped\n");
+#endif
+    return errorCnt;
+}
+
+
+int test_biq_df1_q15(int blockSize, int nbCascade, int dump, prof * p)
+{
+    int             errorCnt = 0;
+#if defined(ARM_MATH_MVEF)
+    int             alignOffs = 0;
+
+    SETUP_BIQUAD_DF1(q15_t, q15, nbCascade, blockSize, alignOffs);
+
+    arm_biquad_cascade_df1_init_q15(&SRef, nbCascade, pcoefs, pstateRef POSTSHIFT_q15);
+    arm_biquad_cascade_df1_init_q15(&SOpt, nbCascade, pcoefs, pstateOpt POSTSHIFT_q15);
+
+    pcoefs = coefs;
+    GEN_BIQUAD_LPF_FX(q15_t, nbCascade, pcoefs, 13);
+
+    set_input(RAND, blockSize, pInput, powf(2.0, -10.0), q15_t);
+
+    RUN_BIQUAD_DF1(arm_biquad_cascade_df1_q15, 1);
+
+    DUMP_ON_BIQUAD_DF1_ERROR("%d");
+
+#else
+    printf("skipped\n");
+#endif
+    return errorCnt;
+}
 
 int test_fir_f32(int blockSize, int nbTaps, int dump, prof * p)
 {
@@ -149,27 +273,35 @@ int main(void)
 {
     prof            p;
     test            testlist[] = {
+        /* FIR filtering */
         {test_fir_f32, "firf32", 64, 256, 8, 32},
         {test_fir_f16, "firf16", 64, 256, 8, 32},
         {test_fir_q31, "firq31", 64, 256, 8, 32},
         {test_fir_q15, "firq15", 64, 256, 8, 32},
+        /* DF1 Biquad filtering */
+        {test_biq_df1_f32, "biqdf1_f32", 64, 256, 1, 4},
+        {test_biq_df1_f16, "biqdf1_f16", 64, 256, 1, 4},
+        {test_biq_df1_q31, "biqdf1_q31", 64, 256, 1, 4},
+        {test_biq_df1_q15, "biqdf1_q15", 64, 256, 1, 4},
+
         {NULL, NULL}
     };
     test           *curtest = testlist;
 
     init_cycle_counter(false);
 
-		printf("test, blksz, filter sz., cmsis-dsp, opt., status\n");
-    while (curtest->f != NULL) {
-        testfct         curfct = curtest->f;
-        for (int blksz = curtest->minBlkSize; blksz <= curtest->maxBlkSize; blksz <<= 1) {
-            for (int filtsz = curtest->minFltSize; filtsz <= curtest->maxFltSize; filtsz <<= 1) {
-                int             errorCnt = curfct(blksz, filtsz, 0, &p);
 
-                printf("%s, %d, %d, %d, %d, %s\n", curtest->name, blksz, filtsz, (uint32_t) p.ref,
-                       (uint32_t) p.opt, errorCnt == 0 ? "OK" : "KO");
-            }
+    printf("test, blksz, filter sz., cmsis-dsp, opt., status\n");
+    while (curtest->f != NULL) {
+    testfct         curfct = curtest->f;
+    for (int blksz = curtest->minBlkSize; blksz <= curtest->maxBlkSize; blksz <<= 1) {
+        for (int filtsz = curtest->minFltSize; filtsz <= curtest->maxFltSize; filtsz <<= 1) {
+        int             errorCnt = curfct(blksz, filtsz, 0, &p);
+
+        printf("%s, %d, %d, %d, %d, %s\n", curtest->name, blksz, filtsz, (uint32_t) p.ref,
+               (uint32_t) p.opt, errorCnt == 0 ? "OK" : "KO");
         }
-        curtest++;
+    }
+    curtest++;
     }
 }
